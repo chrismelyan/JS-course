74e65b36c12dcf1423023873932924ead9c8d2d6
chrismelyan <chris.shelpuk@gmail.com>
2021-12-29 14:31:56 +0100

    ex.2
    ex.2 changes

--- Hangman.js
+++ Hangman.js
@@ -1,28 +1,38 @@
+// Создаем массив со словами
 var words = ["программа", "макака", "прекрасный", "оладушек", "кирпич", "голова", "небоскреб"];
+// Выбираем случайное слово
 var word = words[Math.floor(Math.random() * words.length)];
-
+// Создаем итоговый массив
 var answerArray = [];
 for (var i = 0; i < word.length; i++) {
 answerArray[i] = "_";
 }
 
 var remainingLetter = word.length;
-
+// Итоговый массив
 while(remainingLetter > 0) {
+    // Показываем состояние игры
 alert(answerArray.join(" "));
+
+// Запрашиваем вариант ответа 
 var guess = prompt("Угадай букву");
 if (guess === null) {
+    // Выходим из игрового цикла
 break;
 } else if (guess.length !== 1) {
 alert ("Введи одну букву!");
 } else {
-for (var j =0; j < word.length; j++){
+    guess = guess.toLocaleLowerCase();
+    // Обновляем состояние игры
+for (var j = 0; j < word.length; j++){
 if (word[j] === guess) {
 answerArray[j] = guess;
 remainingLetter--;
 }
 }
 }
+// Конец игрового цикла
 }
+// Отображаем ответ и поздравляем игрока
 alert (answerArray.join(" "));
-alert ("Отлично!");
+alert ("Отлично! Было загадано слово " + word");
