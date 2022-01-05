b75e7c976064c93ae979e8288eb4c6fba647b398
chrismelyan <chris.shelpuk@gmail.com>
2022-01-05 13:44:50 +0100

    new prequal var

--- unit 5.js
+++ unit 5.js
@@ -8,6 +8,36 @@ var taxi = {
     mileage: 281341
 };
 
+var cadi = {
+    make: "GM",
+    model: "Cadillac",
+    year: 1955,
+    color: "tan",
+    passengers: 5,
+    convertible: false,
+    mileage: 12892
+};
+
+var chevy = {
+    make: "Chevy",
+    model: "Bel Air",
+    year: 1957,
+    color: "red",
+    passengers: 2,
+    convertible: false,
+    mileage: 1021
+};
+
+var fiat = {
+    make: "Fiat",
+    model: "500",
+    year: 1957,
+    color: "Medium Blue",
+    passengers: 2,
+    convertible: false,
+    mileage: 88000
+};
+
 function prequal(car) {
     if (car.mileage > 10000) {
         return false;
@@ -23,4 +53,28 @@ if (worthALook) {
     console.log("You gotta check out this " + taxi.make + " " + taxi.model);
 } else {
     console.log("You should really pass on the " + taxi.make + " " + taxi.model);
+}
+
+var worthALook1 = prequal(fiat);
+
+if (worthALook1) {
+    console.log("You gotta check out this " + fiat.make + " " + fiat.model);
+} else {
+    console.log("You should really pass on the " + fiat.make + " " + fiat.model);
+}
+
+var worthALook2 = prequal(chevy);
+
+if (worthALook2) {
+    console.log("You gotta check out this " + chevy.make + " " + chevy.model);
+} else {
+    console.log("You should really pass on the " + chevy.make + " " + chevy.model);
+}
+
+var worthALook3 = prequal(cadi);
+
+if (worthALook3) {
+    console.log("You gotta check out this " + cadi.make + " " + cadi.model);
+} else {
+    console.log("You should really pass on the " + cadi.make + " " + cadi.model);
 }
