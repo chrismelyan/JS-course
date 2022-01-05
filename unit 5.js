5875858e79498ef92d618074bdb46bb8087cf970
chrismelyan <chris.shelpuk@gmail.com>
2022-01-05 13:21:57 +0100

    new file

---
+++ unit 5.js
@@ -0,0 +1,26 @@
+let taxi = {
+    make: "Webville Motors",
+    model: "Taxi",
+    year: 1955,
+    color: "yellow",
+    passengers: 4,
+    convertible: false,
+    mileage: 281341
+};
+
+function prequal(car) {
+    if (car.mileage > 10000) {
+        return false;
+    } else if (car.year > 1960) {
+        return false;
+    }
+    return true;
+}
+
+let worthALook = prequal(taxi);
+
+if (worthALook) {
+    console.log("You gotta check out this " + taxi.make + " " + taxi.model);
+} else {
+    console.log("You should really pass on the " + taxi.make + " " + taxi.model);
+}
